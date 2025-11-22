8770d5767069dd2ddce14b1d4a44147ae35c712c
baby6969 <yafetzekarias23@fmail.com>
2025-11-23 02:02:30 +0300

    fix bug

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,15 +24,16 @@ const director1: Director = {
 
 console.log(director1);
 
+
 // Define the function interface
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  (teacher: { firstName: string; lastName: string }): string;
 }
 
 // Implement the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName} ${lastName}`;
+const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
 }
 
 // Example usage
-console.log(printTeacher("John", "Doe")); // Output: John Doe
+console.log(printTeacher({ firstName: "John", lastName: "Doe" })); // Output: J. Doe
